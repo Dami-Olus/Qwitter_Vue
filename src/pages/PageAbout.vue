7832cd1cc246ed758e1c414c49384cf8276ee582
Damola Olusakin <olusakinadedamola@gmail.com>
2022-10-09 17:45:36 +0100

    added about page and content

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -1,6 +1,10 @@
 <template>
-  <q-page >
-    <h5>About</h5>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Qwitter</h4>
+    <div class="text-body1">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo odio aenean sed adipiscing diam donec adipiscing tristique risus. Ac tortor vitae purus faucibus ornare. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus. Nullam vehicula ipsum a arcu cursus vitae. Sagittis vitae et leo duis. Pulvinar mattis nunc sed blandit. Neque egestas congue quisque egestas diam in arcu. Vestibulum sed arcu non odio euismod lacinia at quis risus. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Sodales ut etiam sit amet nisl purus in mollis. </p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo odio aenean sed adipiscing diam donec adipiscing tristique risus. Ac tortor vitae purus faucibus ornare. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus. Nullam vehicula ipsum a arcu cursus vitae. Sagittis vitae et leo duis. Pulvinar mattis nunc sed blandit. Neque egestas congue quisque egestas diam in arcu. Vestibulum sed arcu non odio euismod lacinia at quis risus. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Sodales ut etiam sit amet nisl purus in mollis. </p>
+    </div>
   </q-page>
 </template>
 
