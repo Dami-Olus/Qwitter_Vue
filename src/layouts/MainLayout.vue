378e033e15aa34af7a85af2bc2b69810f34f7b7f
Damola Olusakin <olusakinadedamola@gmail.com>
2022-10-09 17:40:15 +0100

    render page name to header

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,7 +5,7 @@
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
         <q-toolbar-title class="text-weight-bold">
-       <span class="gt-sm"> Qwitter </span>
+       <span class="gt-sm"> {{$route.name}} </span>
        <q-icon
         class="q-pa-md lt-md header-icon"
         name="fa-solid fa-dove"
@@ -30,7 +30,7 @@
         color="primary"
       />
       <q-list>
-        <q-item to="/" clickable v-ripple>
+        <q-item to="/" clickable v-ripple exact>
           <q-item-section avatar>
             <q-icon name="home" size="md" />
           </q-item-section>
@@ -38,7 +38,7 @@
           <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
         </q-item>
 
-        <q-item to="/about" clickable v-ripple>
+        <q-item to="/about" clickable v-ripple exact>
           <q-item-section avatar>
             <q-icon name="help" size="md" />
           </q-item-section>
