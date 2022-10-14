720b21cc650fda779de2722e4106edba1a9ee9e4
Damola Olusakin <olusakinadedamola@gmail.com>
2022-10-14 05:29:41 +0100

    added input feild

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,13 +1,52 @@
 <template>
-  <q-page >
-    <h5>Home</h5>
+  <q-page>
+    <div class="q-py-lg q-px-md">
+      <q-input
+        bottom-slots
+        v-model="newQweetContent"
+        class="new-qweet"
+        placeholder="What's happening"
+        counter
+        maxlength="280"
+        autogrow
+      >
+        <template v-slot:before>
+          <q-avatar size="xl">
+            <img src="https://bit.ly/3EBR5Qp" />
+          </q-avatar>
+        </template>
+
+        <template v-slot:after>
+          <q-btn
+            :disable="!newQweetContent"
+            unelevated
+            rounded
+            color="primary"
+            label="Qweet"
+            no-caps
+          />
+        </template>
+      </q-input>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'PageHome'
-})
+  name: "PageHome",
+  data() {
+    return {
+      newQweetContent: "",
+    };
+  },
+});
 </script>
+
+<style lang="sass">
+.new-qweet
+  textarea
+    font-size: 19px
+    line-height: 1.4 !important
+</style>
